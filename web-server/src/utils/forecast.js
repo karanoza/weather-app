b189f11df19e65ed0621b6765e2971451e6e33b5
Karan Oza <50609276+karanoza@users.noreply.github.com>
2020-10-09 19:14:50 +0530

    Create forecast.js

---
+++ web-server/src/utils/forecast.js
@@ -0,0 +1,39 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=69808d7dddadeb0437ae9d857d4b2604&query=" +
+    latitude +
+    "," +
+    longitude +
+    "&units=f";
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to Weather services!", undefined);
+    } else if (body.error) {
+      callback("Unable to find loction!", undefined);
+    } else {
+      callback(
+        undefined,
+        (bro =
+          body.current.weather_descriptions[0] +
+          ". It is currently " +
+          body.current.temperature +
+          " degrees out. It feels like " +
+          body.current.feelslike +
+          " degrees out"),
+        console.log(
+          body.current.weather_descriptions[0] +
+            ". It is currently " +
+            body.current.temperature +
+            " degrees out. It feels like " +
+            body.current.feelslike +
+            " degrees out"
+        )
+      );
+    }
+  });
+};
+
+module.exports = forecast;
